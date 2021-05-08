d8cbb35353279630536b1ce8722d63f2895c0f32
Tranthilananh1803 <tranlananhquyet2318@gmail.com>
2021-05-08 07:18:47 +0700

    commit

--- PTPMQLbtl/Controllers/NhacungcapsController.cs
+++ PTPMQLbtl/Controllers/NhacungcapsController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.Entity;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -113,6 +117,118 @@ namespace PTPMQLbtl.Controllers
             db.Nhacungcaps.Remove(nhacungcap);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+        //upload file
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            //dat ten cho file
+            string _FileName = "NhaCC.xlsx";
+
+            //duong dan luu file
+            string _path = Path.Combine(Server.MapPath("~/Uploads/Excels"), _FileName);
+            //luu file len server
+            file.SaveAs(_path);
+
+            //doc du lieu tu file excel
+            DataTable dt = ReadDataFromExcelFile(_path);
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+               Nhacungcap kh = new Nhacungcap();
+                kh.TenNCC = dt.Rows[i][0].ToString();
+                kh.Diachi = dt.Rows[i][1].ToString();
+                kh.Sodienthoai = dt.Rows[i][2].ToString();
+                db.Nhacungcaps.Add(kh);
+                db.SaveChanges();
+            }
+
+            try
+            {
+                //upload file thanh cong va file co du lieu
+                if (file.ContentLength > 0)
+                {
+
+                    //CopyDataByBulk(ReadDataFromExcelFile(_path));
+                    ViewBag.ThongBao = "Upload file thanh cong";
+                }
+            }
+            catch (Exception ex)
+            {
+                //neu upload file that bai
+                ViewBag.ThongBao = "Upload file that bai";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // private void CopyDataByBulk(object p)
+
+        //  throw new NotImplementedException();
+
+        //doc file excel tra ve du lieu dang datatable
+        public DataTable ReadDataFromExcelFile(string filepath)
+        {
+            string connectionString = "";
+            string fileExtention = filepath.Substring(filepath.Length - 4).ToLower();
+            if (fileExtention.IndexOf("xlsx") == 0)
+            {
+                connectionString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source =" + filepath + ";Extended Properties=\"Excel 12.0 Xml;HDR=NO\"";
+            }
+            else if (fileExtention.IndexOf("xls") == 0)
+            {
+                connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filepath + ";Extended Properties=Excel 8.0";
+            }
+
+            // Tạo đối tượng kết nối
+            OleDbConnection oledbConn = new OleDbConnection(connectionString);
+            DataTable data = null;
+            try
+            {
+                // Mở kết nối
+                oledbConn.Open();
+
+                // Tạo đối tượng OleDBCommand và query data từ sheet có tên "Sheet1"
+                OleDbCommand cmd = new OleDbCommand("SELECT * FROM [Sheet1$]", oledbConn);
+
+                // Tạo đối tượng OleDbDataAdapter để thực thi việc query lấy dữ liệu từ tập tin excel
+                OleDbDataAdapter oleda = new OleDbDataAdapter();
+
+                oleda.SelectCommand = cmd;
+
+                // Tạo đối tượng DataSet để hứng dữ liệu từ tập tin excel
+                DataSet ds = new DataSet();
+
+                // Đổ đữ liệu từ tập excel vào DataSet
+                oleda.Fill(ds);
+
+                data = ds.Tables[0];
+            }
+            catch
+            {
+            }
+            finally
+            {
+                // Đóng chuỗi kết nối
+                oledbConn.Close();
+            }
+            return data;
+        }
+
+        //copy large data from datatable to sqlserver
+        private void CopyDataByBulk(DataTable dt)
+        {
+            //lay ket noi voi database luu trong file webconfig
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["PTPMQLDB"].ConnectionString);
+            SqlBulkCopy bulkcopy = new SqlBulkCopy(con);
+            bulkcopy.DestinationTableName = "Nhacungcaps";
+            bulkcopy.ColumnMappings.Add(0, "TenNCC");
+            bulkcopy.ColumnMappings.Add(1, "Diachi");
+            bulkcopy.ColumnMappings.Add(2, "Sodienthoai");
+
+            con.Open();
+            bulkcopy.WriteToServer(dt);
+            con.Close();
         }
 
         protected override void Dispose(bool disposing)
